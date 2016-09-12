6340bcc9f11fd7a005691e39efcfcbb48f01f072
William Youngc <william@werl.me>
2016-09-12 15:37:49 -0400

    Add Purchase model
    ran `rails generate model purchases state:integer user:references`
    
    ran `rails db:migrate`
    
    added relations to User model
    
    Purchase model will be used to represent a purchase (group of
    transactions) that is made by a user. Additionally it stores the state
    of the purchase.

--- SemenHubDashboard/app/models/user.rb
+++ SemenHubDashboard/app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   belongs_to :payee_address, class_name: 'Address', required: false
 
   has_one :commission, class_name: 'Commission' # class_name required due to rails bug
+
+  has_many :purchases
 end
--- SemenHubDashboard/db/schema.rb
+++ SemenHubDashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160912192250) do
+ActiveRecord::Schema.define(version: 20160912193420) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "line1"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 20160912192250) do
     t.string   "alpha_3",    limit: 3, default: ""
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
+  end
+
+  create_table "purchases", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_purchases_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|

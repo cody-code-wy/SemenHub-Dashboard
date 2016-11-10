a1636159b423082e95dc226055b28b0fe930a9b8
Peter Lewis <peter@werl.me>
2016-11-09 19:35:52 -0700

    Add storage_facilities new and create routes with pages

--- SemenHubDashboard/app/controllers/storage_facilities_controller.rb
+++ SemenHubDashboard/app/controllers/storage_facilities_controller.rb
@@ -1,2 +1,10 @@
 class StorageFacilitiesController < ApplicationController
+
+  def new
+    @facility = StorageFacility.new
+  end
+
+  def create
+    @facility = StorageFacility.new(facility_params)
+  end
 end

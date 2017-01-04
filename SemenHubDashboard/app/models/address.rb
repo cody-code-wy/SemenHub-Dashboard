a0b4aac8211edaf0778c9df1588ce6391bf7c879
Peter Lewis <peter@werl.me>
2017-01-03 22:12:52 -0700

    add a location column to the storage_facilities index and a get_location
    helper in the Address model

--- SemenHubDashboard/app/models/address.rb
+++ SemenHubDashboard/app/models/address.rb
@@ -2,4 +2,9 @@ class Address < ApplicationRecord
   belongs_to :country, foreign_key: 'alpha_2', primary_key: 'alpha_2'
 
   validates_presence_of :line1, :city, :region, :postal_code
+
+  def get_location
+    "#{city}, #{region}"
+  end
+
 end

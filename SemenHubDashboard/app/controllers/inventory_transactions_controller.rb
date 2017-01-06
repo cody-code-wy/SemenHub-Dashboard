87d9290d95aadb11bcf0a06e36b26186c08d874c
Peter Lewis <peter@werl.me>
2017-01-06 13:09:26 -0700

    add create logic

--- SemenHubDashboard/app/controllers/inventory_transactions_controller.rb
+++ SemenHubDashboard/app/controllers/inventory_transactions_controller.rb
@@ -3,4 +3,35 @@ class InventoryTransactionsController < ApplicationController
   def new
     @transaction = InventoryTransaction.new
   end
+
+  def create
+    @transaction = InventoryTransaction.new(get_params)
+
+    put_data_in_transaction
+
+    if @transaction.save
+      redirect_to @transaction
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def get_params
+    params.require(:inventory_transaction).permit(
+      :quantity, :private, :semen_type, :price_per_unit, :semen_count, :semen_count
+    )
+  end
+
+  def get_secondary_params
+    params.require(:inventory_transaction).permit(
+      :animal, :storageFacility
+    )
+  end
+
+  def put_data_in_transaction
+    @transaction.animal = Animal.find(get_secondary_params[:animal])
+    @transaction.storageFacility = StorageFacility.find(get_secondary_params[:storageFacility])
+  end
 end

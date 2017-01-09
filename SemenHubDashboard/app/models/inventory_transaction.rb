8b415b38cbe041159e57136c1b58e79bbd329e23
Peter Lewis <peter@werl.me>
2017-01-08 20:18:28 -0700

    Add relation between User and InventoryTransaction
    InventoryTransaction has a Seller (seller_id) that links to a User

--- SemenHubDashboard/app/models/inventory_transaction.rb
+++ SemenHubDashboard/app/models/inventory_transaction.rb
@@ -1,6 +1,7 @@
 class InventoryTransaction < ApplicationRecord
   belongs_to :animal
   belongs_to :storageFacility
+  belongs_to :seller, class_name: 'User'
 
   has_many :purchaseTransactions, foreign_key: 'inventoryTransaction_id' # Rails generates FK as 'inventory_transaction_id' but the migration created 'inventoryTransaction_id'
   has_many :purchases, through: :purchaseTransactions
--- SemenHubDashboard/app/models/user.rb
+++ SemenHubDashboard/app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
 
   has_many :purchases
   has_many :animals, foreign_key: 'owner_id'
+  has_many :inventory_transactions, foreign_key: 'seller_id'
 
   validates_presence_of :first_name, :last_name, :email, :phone_primary
 

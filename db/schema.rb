73b61da4d5859d2340a57770b02ec2b110cbb27e
William Young <william@werl.me>
2017-02-06 14:08:02 -0600

    Create sku table duplicating inventoryTransaction
    ran `rails g migration AddSKUSTable`
    
    Added migration code
    
    ran `rails db:migrate`

--- db/schema.rb
+++ db/schema.rb
@@ -139,6 +139,21 @@ ActiveRecord::Schema.define(version: 20170211054144) do
     t.index ["inventoryTransaction_id"], name: "index_ships_tos_on_inventoryTransaction_id", using: :btree
   end
 
+  create_table "skus", force: :cascade do |t|
+    t.boolean  "private"
+    t.integer  "semen_type"
+    t.decimal  "price_per_unit"
+    t.integer  "semen_count"
+    t.integer  "animal_id"
+    t.integer  "storagefacility_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.integer  "seller_id"
+    t.decimal  "cost_per_unit"
+    t.index ["animal_id"], name: "index_sku_on_animal_id", using: :btree
+    t.index ["storagefacility_id"], name: "index_sku_on_storagefacility_id", using: :btree
+  end
+
   create_table "storage_facilities", force: :cascade do |t|
     t.string   "phone_number"
     t.string   "website"

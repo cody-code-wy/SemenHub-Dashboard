b88ed31a6c0c0d6ff47ad7dd3b859e1a80aee562
William Young <william@werl.me>
2017-02-13 00:16:48 -0600

    Add relations to inventory transactions and skus
    Also move all relations to the aproprate models, along with validations.

--- app/models/inventory_transaction.rb
+++ app/models/inventory_transaction.rb
@@ -1,14 +1,11 @@
 class InventoryTransaction < ApplicationRecord
-  belongs_to :animal
-  belongs_to :storageFacility
-  belongs_to :seller, class_name: 'User'
-
-  has_many :purchaseTransactions, foreign_key: 'inventoryTransaction_id' # Rails generates FK as 'inventory_transaction_id' but the migration created 'inventoryTransaction_id'
+ has_many :purchaseTransactions, foreign_key: 'inventoryTransaction_id' # Rails generates FK as 'inventory_transaction_id' but the migration created 'inventoryTransaction_id'
   has_many :purchases, through: :purchaseTransactions
+  belongs_to :sku
 
   has_many :shipsTo, foreign_key: 'inventoryTransaction_id'
   has_many :countries, through: :shipsTo
   
-  validates_presence_of :quantity, :semen_type, :price_per_unit, :semen_count
 
+  validates_presence_of :quantity
 end

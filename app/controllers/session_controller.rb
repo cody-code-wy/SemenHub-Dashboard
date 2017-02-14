3e9a72b023bf1924d326c94a92cee45eb9e6b4c3
William Young <william@werl.me>
2017-02-14 16:52:48 -0600

    fixup! Add sessions controller w/ session new function

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,9 +2,6 @@ class SessionController < ApplicationController
   def new
     @session = {sessionid: SecureRandom.uuid}
 
-    $redis.set @session[:sessionid], [].to_json
-    $redis.expire @session[:sessionid], $redis_timeout
-
     render json: @session
   end
 

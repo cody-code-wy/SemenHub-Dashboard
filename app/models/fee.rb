023aed7421853c0608f38465b9e14fb2fde612dc
Peter Lewis <peter@werl.me>
2017-02-24 21:30:35 -0700

    add the model data to fees and storage_facility

--- app/models/fee.rb
+++ app/models/fee.rb
@@ -1,2 +1,13 @@
 class Fee < ApplicationRecord
+  belongs_to :storage_facility
+  
+  enum fee_type: {
+    release: 0,
+    handeling: 1,
+    tank_rental: 2,
+    packaging_fee: 3
+  }
+
+  validates_presence_of :price, :fee_type
+
 end

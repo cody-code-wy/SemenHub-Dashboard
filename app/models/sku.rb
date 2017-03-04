f2234f73e98286d156680824fad23480928a0981
William Young <william@werl.me>
2017-03-04 15:43:05 -0700

    Add enums to sku model and update views

--- app/models/sku.rb
+++ app/models/sku.rb
@@ -6,4 +6,7 @@ class Sku < ApplicationRecord
   has_many :inventory_transaction
 
   validates_presence_of :semen_type, :price_per_unit, :semen_count
+
+  enum semen_type: ["Conventional", "Male Sexed", "Female Sexed"]
+  enum semen_count: ["2.1", "5.0"]
 end

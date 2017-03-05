ba3fca4c9dc811b0d71d9c4ad9204d4c54941791
William Young <william@werl.me>
2017-03-04 19:25:30 -0700

    Add sku relation, and enum for state

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -3,4 +3,7 @@ class Purchase < ApplicationRecord
 
   has_many :purchase_transactions
   has_many :inventory_transactions, through: :purchase_transactions
+  has_many :skus, through: :inventory_transactions
+
+  enum state: ["problem", "created", "invoiced", "paid", "preparing for shipment", "shipped", "delivered", "canceled", "refunded"]
 end

8b48aa7e0562f5c41f09b88d665fa14596039684
William Young <william@werl.me>
2017-03-06 11:35:44 -0700

    Add util methods to sku and updatte cart show view

--- app/models/sku.rb
+++ app/models/sku.rb
@@ -9,4 +9,13 @@ class Sku < ApplicationRecord
 
   enum semen_type: ["Conventional", "Male Sexed", "Female Sexed"]
   enum semen_count: ["2.1", "5.0"]
+
+  def similar # get 'similar' skus, with least quantity first
+    Sku.where(private: private, semen_type: semen_type, price_per_unit: price_per_unit, semen_count: semen_count, animal: animal, storagefacility: storagefacility).joins(:inventory_transaction).group(:id).order("sum(quantity) asc")
+  end
+
+  def quantity
+    inventory_transaction.sum(:quantity)
+  end
+
 end

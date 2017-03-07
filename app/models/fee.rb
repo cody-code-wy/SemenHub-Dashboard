1e65d53efd1047168a928e53ddfff2f6bd7fc70e
Peter Lewis <peter@werl.me>
2017-03-06 22:21:36 -0700

    add minor fixes
    change 'edit' to 'view' in fees#index
    change symbols to strings in Fee#fee_type enum

--- app/models/fee.rb
+++ app/models/fee.rb
@@ -2,10 +2,10 @@ class Fee < ApplicationRecord
   belongs_to :storage_facility
 
   enum fee_type: {
-    release: 0,
-    handeling: 1,
-    tank_rental: 2,
-    packaging_fee: 3
+    'Release': 0,
+    'Handling': 1,
+    'Tank Rental': 2,
+    'Packaging Fee': 3
   }
 
   validates_presence_of :price, :fee_type

79db7c495c3756a078f3ccf09b646cc08011e6c6
William Young <william@werl.me>
2017-03-08 10:27:34 -0700

    Add relation to storagefacilities

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -4,6 +4,7 @@ class Purchase < ApplicationRecord
   has_many :purchase_transactions
   has_many :inventory_transactions, through: :purchase_transactions
   has_many :skus, through: :inventory_transactions
+  has_many :storagefacilities, through: :skus
 
   enum state: ["problem", "created", "invoiced", "paid", "preparing for shipment", "shipped", "delivered", "canceled", "refunded"]
 

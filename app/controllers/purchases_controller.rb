2e6f4d33174a1d2ce8fa0c233b44c2753f120a39
William Young <william@werl.me>
2017-03-16 12:41:48 -0500

    Add animal name to purchase show

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -7,6 +7,7 @@ class PurchasesController < ApplicationController
 
   def show
     @purchase = Purchase.find(params[:id])
+    @purchase.build_shipment if @purchase.shipment.blank?
     @partial = @purchase.state if lookup_context.exists?(@purchase.state, _prefixes, true)
   end
 

7b574cdab84715dd2089f8aae64b8836b139e2d6
William Young <william@werl.me>
2017-03-15 19:47:05 -0500

    Add address validator helpers
    this is not a validator yet though

--- app/models/address.rb
+++ app/models/address.rb
@@ -7,4 +7,25 @@ class Address < ApplicationRecord
     "#{city}, #{region}"
   end
 
+  def get_address_validator(name = "Resident")
+    AddressValidator::Address.new(
+      name: name,
+      street1: line1,
+      street2: line2,
+      city: city,
+      state: region,
+      zip: postal_code,
+      country: alpha_2
+    )
+  end
+
+  def get_validator_response(name = "Resident")
+    response ||= $validator.validate(get_address_validator(name))
+    response
+  end
+
+  def is_valid_address(name = "Resident")
+    get_validator_response(name).valid?
+  end
+
 end

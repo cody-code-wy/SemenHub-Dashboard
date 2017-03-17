0c4e3f778d6bf8b5aaaf1476f6f35adbb4b00b6a
William Young <william@werl.me>
2017-03-16 23:45:47 -0500

    Hotfix purchases controller method name

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -17,7 +17,7 @@ class PurchasesController < ApplicationController
     @partial = @purchase.state if lookup_context.exists?(@purchase.state, _prefixes, true)
   end
 
-  def update
+  def get_address
     @purchase = Purchase.find(params[:id])
     @storage = StorageFacility.find_by_address_id(params[:shipment][:address_id])
     @purchase.shipment = Shipment.new(address: @storage.address, account_name: current_user.get_name )

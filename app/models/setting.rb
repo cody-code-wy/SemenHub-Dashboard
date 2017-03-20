225722dd319f131f12557bd97fa60e20072cb357
William Young <william@werl.me>
2017-03-20 18:55:19 -0500

    Add defaults setting

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,8 +1,14 @@
 class Setting < ApplicationRecord
+  self.primary_key = 'setting'
 
   enum setting: {add_to_cart_buttons_enabled: 0, checkout_button_enabled: 1, remote_account_access_buttons_enabled: 2, credit_card_processing_enabled: 3}
 
   validates_presence_of :setting
   validates_uniqueness_of :setting
+
+  def self.get_setting(selected_setting)
+    setting = where(setting: selected_setting).first
+    setting ||= new(setting: selected_setting, value: DEFAULT_SETTINGS[selected_setting.to_s])
+  end
 
 end

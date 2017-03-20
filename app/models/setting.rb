4043e69fa6a83938fba0fdd3a0c2ae2012ad261a
Peter Lewis <peter@werl.me>
2017-03-20 15:39:10 -0600

    add enum and validations to setting model

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,7 +1,8 @@
 class Setting < ApplicationRecord
 
-  enum setting = [add_to_cart_buttons_enabled: 0, checkout_button_enabled: 1, remote_account_access_buttons_enabled: 2, credit_card_processing_enabled: 3]
+  enum setting: {add_to_cart_buttons_enabled: 0, checkout_button_enabled: 1, remote_account_access_buttons_enabled: 2, credit_card_processing_enabled: 3}
 
   validates_presence_of :setting
+  validates_uniqueness_of :setting
 
 end

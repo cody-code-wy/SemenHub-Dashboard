54282e3a5598777d305c9674f1ffaa488f6f97a6
William Young <william@werl.me>
2017-04-01 16:31:57 -0500

    Add administrative notice email

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -2,6 +2,12 @@ class PurchaseMailer < ApplicationMailer
   default from: 'default@semenhub.shop'
   layout 'mailer'
 
+
+  def administrative_notice(purchase)
+    @purchase = purchase
+    mail(to: 'semenhub@gmail.com', subject: "Purchases Administrative Notice #{@purchase.user.get_name}")
+  end
+
   def incomplete_invoice(purchase)
     @purchase = purchase
     mail(to: @purchase.user.email, subject: 'Your SemenHub Order Update')
--- test/mailers/previews/purchase_mailer_preview.rb
+++ test/mailers/previews/purchase_mailer_preview.rb
@@ -1,6 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/purchase_mailer
 class PurchaseMailerPreview < ActionMailer::Preview
 
+  def administrative_notice
+    PurchaseMailer.administrative_notice(Purchase.last)
+  end
+
   def incomplete_invoice
     PurchaseMailer.incomplete_invoice(Purchase.last)
   end

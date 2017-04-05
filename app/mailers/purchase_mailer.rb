fb48fd9fa8aafe964ec72ff1d52a74343686bca2
William Young <william@werl.me>
2017-04-04 20:12:44 -0500

    add purchase number on every subject

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -5,36 +5,36 @@ class PurchaseMailer < ApplicationMailer
 
   def administrative_notice(purchase)
     @purchase = purchase
-    mail(to: 'semenhub@gmail.com', subject: "Purchases Administrative Notice #{@purchase.user.get_name}")
+    mail(to: 'semenhub@gmail.com', subject: "Purchase #{@purchase.id} Administrative Notice #{@purchase.user.get_name}")
   end
 
   def incomplete_invoice(purchase)
     @purchase = purchase
-    mail(to: @purchase.user.email, subject: 'Your SemenHub Order Update')
+    mail(to: @purchase.user.email, subject: "Your SemenHub Order Update (purchase #{@purchase.id})")
   end
 
   def invoice(purchase)
     @purchase = purchase
-    mail(to: @purchase.user.email, subject: 'Your SemenHub Invoice')
+    mail(to: @purchase.user.email, subject: "Your SemenHub Invoice (purchase #{@purchase.id})")
   end
 
   def receipt(purchase)
     @purchase = purchase
-    mail(to: @purchase.user.email, subject: 'Your SemenHub Receipt')
+    mail(to: @purchase.user.email, subject: "Your SemenHub Receipt (purchase #{@purchase.id})")
   end
 
   def purchase_order(purchase, seller)
     @purchase = purchase
     @seller = seller
-    @order_items = purchase.inventory_transactions.where(sku: @seller.skus)
-    mail(to: @seller.email, subject: 'New SemenHub Purchase Order')
+    @order_items = @purchase.inventory_transactions.where(sku: @seller.skus)
+    mail(to: @seller.email, subject: "New SemenHub Purchase Order (purchase #{@purchase.id})")
   end
 
   def shipping_order(purchase, storagefacility)
     @purchase = purchase
     @storagefacility = storagefacility
-    @order_items = purchase.inventory_transactions.where(sku: @storagefacility.skus)
-    mail(to: @storagefacility.email, subject: 'New SemenHub Shipping Order')
+    @order_items = @purchase.inventory_transactions.where(sku: @storagefacility.skus)
+    mail(to: @storagefacility.email, subject: "New SemenHub Shipping Order (purchase #{@purchase.id})")
   end
 
 end

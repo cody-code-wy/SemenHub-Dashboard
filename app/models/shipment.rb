e19df421baf12aa12dc3cee21698201784a3ad33
William Young <william@werl.me>
2017-04-20 19:44:08 -0500

    Update shipment model relations

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,6 +1,7 @@
 class Shipment < ApplicationRecord
   belongs_to :purchase, touch: true
   belongs_to :address
+  belongs_to :origin_address, class_name: 'Address'
 
   enum shipping_provider: {
     'UPS': 0,

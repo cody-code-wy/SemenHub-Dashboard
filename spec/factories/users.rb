d83f396c621ee3716c5cbeeb070c48dc2e0d20bd
William Young <william@werl.me>
2017-11-19 18:17:07 -0600

    Add User tests and Validations

---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    spouse_name { Faker::Name.first_name }
+    email { Faker::Internet.unique.email }
+    phone_primary { Faker::PhoneNumber.phone_number }
+    phone_secondary { Faker::PhoneNumber.phone_number }
+    website { Faker::Internet.domain_name }
+    mailing_address { FactoryBot.build(:address) }
+    billing_address { FactoryBot.build(:address) }
+    payee_address { FactoryBot.build(:address) }
+    password { Faker::Internet.password(8) }
+    password_confirmation { password }
+    temp_pass false
+  end
+end

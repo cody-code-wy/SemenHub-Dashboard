65028fcb5e03a31895cd04c6ba7c551a907f79dc
William Young <william@werl.me>
2017-11-20 05:31:56 -0600

    Added more Role tests

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :role do
     name { Faker::Lorem.word }
+
+    trait :with_permissions do
+      permissions { [ FactoryBot.build(:permission), FactoryBot.build(:permission) ] }
+    end
+
+    trait :with_users do
+      users { [ FactoryBot.build(:user), FactoryBot.build(:user) ] }
+    end
   end
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -13,9 +13,20 @@ RSpec.describe Role, type: :model do
   end
 
   describe 'Relations' do
-    it 'should have permission_assignments'
-    it 'should have permission thru permission_assignmnets'
-    it 'should belong to role_assignments'
-    it 'should belong to users thru role_assignments'
+    before do
+      @role = FactoryBot.build(:role, :with_permissions, :with_users)
+    end
+    it 'should have permission_assignments' do
+      expect(@role.permission_assignments.first).to be_a PermissionAssignment
+    end
+    it 'should have permission thru permission_assignmnets' do
+      expect(@role.permissions.first).to be_a Permission
+    end
+    it 'should belong to role_assignments' do
+      expect(@role.role_assignments.first).to be_a RoleAssignment
+    end
+    it 'should belong to users thru role_assignments' do
+      expect(@role.users.first).to be_a User
+    end
   end
 end

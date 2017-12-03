c0c1abe9a268471842882fb2e308fbe250acd6d8
William Young <william@werl.me>
2017-12-03 06:24:54 -0600

    Add shipment specs

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -3,6 +3,8 @@ class Shipment < ApplicationRecord
   belongs_to :address
   belongs_to :origin_address, class_name: 'Address'
 
+  validates_presence_of :shipping_provider, :origin_name
+
   enum shipping_provider: {
     'UPS': 0,
     'FedEx': 1

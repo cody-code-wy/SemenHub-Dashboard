c6a5e9905ee331c096e53a948ffc7865001228a3
Peter Lewis <peter@werl.me>
2018-01-31 11:19:45 -0700

    update commission model acording to rails style guide

--- app/models/commission.rb
+++ app/models/commission.rb
@@ -1,6 +1,5 @@
 class Commission < ApplicationRecord
   belongs_to :user
 
-  validates_presence_of :commission_percent
-  validates :commission_percent, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+  validates :commission_percent, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end

8537592ea45bab0602a2069d4c56e16f4042dddd
Peter <peter@werl.me>
2018-01-31 16:45:42 -0700

    update registration model acording to rails style guide

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,8 +1,6 @@
 class Registration < ApplicationRecord
-
   belongs_to :registrar
   belongs_to :animal, touch: true
 
-  validates_presence_of :registration
-
+  validates :registration, presence: true
 end

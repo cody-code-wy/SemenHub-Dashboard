f5639400920625ff9e5f1cc060cc2a688336f3d6
Peter <peter@werl.me>
2018-02-01 13:35:09 -0700

    update role model acording to rails style guide

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,10 +1,9 @@
 class Role < ApplicationRecord
-  validates_presence_of :name
-  validates_uniqueness_of :name
-
   has_many :role_assignments, dependent: :destroy
   has_many :users, through: :role_assignments
-
   has_many :permission_assignments, dependent: :destroy
   has_many :permissions, through: :permission_assignments
+
+  validates_presence_of :name
+  validates_uniqueness_of :name
 end

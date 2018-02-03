da03bc7ebc046409aaef745d7e4d1d1f0c8f289e
William Young <william@werl.me>
2018-02-03 17:27:32 -0600

    Add database cleaner for dev and test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require "#{Rails.root}/db/country_seeds"
-require "#{Rails.root}/db/registrar_seeds"
-require "#{Rails.root}/db/role_permission_seeds"
+if Rails.env == 'development'
+  puts 'Do you want to clear database (y/n)'
+  response = STDIN.gets.chomp
+  if response == 'y'
+    puts 'Clearing Database'
+    require 'database_cleaner'
+    DatabaseCleaner.clean_with(:truncation)
+  else
+    puts 'Database NOT Cleared'
+  end
+end
 
-case Rails.env
-  when "test"
-    require "#{Rails.root}/db/test_user_seeds"
-    require "#{Rails.root}/db/test_seeds"
-  when "development"
-    require "#{Rails.root}/db/test_user_seeds"
-    require "#{Rails.root}/db/dev_seeds"
+if Rails.env == 'test'
+  require 'database_cleaner'
+  DatabaseCleaner.clean_with(:truncation)
+end
+
+ActiveRecord::Base.transaction do
+  require "#{Rails.root}/db/country_seeds"
+  require "#{Rails.root}/db/registrar_seeds"
+  require "#{Rails.root}/db/role_permission_seeds"
+
+  case Rails.env
+    when "test"
+      require "#{Rails.root}/db/test_user_seeds"
+      require "#{Rails.root}/db/test_seeds"
+    when "development"
+      require "#{Rails.root}/db/test_user_seeds"
+      require "#{Rails.root}/db/dev_seeds"
+  end
 end

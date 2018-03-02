e87513c7bdd238a0ee8faeea5728afc17d39b3ff
Peter <peter@werl.me>
2018-03-02 14:34:45 -0700

    add sku regression test

--- spec/features/animal_show_sku_links_spec.rb
+++ spec/features/animal_show_sku_links_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.feature "AnimalShowSkuLinks", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  before do
+    visit '/login'
+    within 'form' do
+      fill_in('email', with: 'test@test.com')
+      fill_in('password', with: 'password')
+      click_on('Login')
+    end
+  end
+  feature 'Check Sku Link' do
+    before do
+      @sku = FactoryBot.create(:sku)
+      visit animal_path(@sku.animal)
+    end
+    it 'should go to Sku page' do
+      click_on("SKU #{@sku.id}")
+      expect(current_path).to eq sku_path(@sku)
+    end
+  end
 end

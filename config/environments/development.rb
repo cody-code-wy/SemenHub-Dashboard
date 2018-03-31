f4d60e850e57c34d671feb8095f3bb76516107a4
William Young <william@werl.me>
2018-03-31 18:59:55 -0500

    Update to new mailtrap inbox

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,16 +1,13 @@
 Rails.application.configure do
   #Setup MailTrap for testing
-  response = RestClient::Resource.new("https://mailtrap.io/api/v1/inboxes.json?api_token=#{ENV['MAILTRAP_API_TOKEN']}", ssl_version: "TLSv1").get
-  first_inbox = JSON.parse(response)[0]
-
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-  :user_name => first_inbox['username'],
-  :password => first_inbox['password'],
-  :address => first_inbox['domain'],
-  :domain => first_inbox['domain'],
-  :port => 2525,
-  :authentication => :cram_md5
+    :user_name => '2c93935b157f19',
+    :password => '26c8eb31038f06',
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
   }
 
 

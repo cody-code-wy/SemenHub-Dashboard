fb6b4f67af2c787f9ba20328018100e3e499586f
William Young <william@werl.me>
2018-05-23 12:37:00 -0500

    remove unused files

--- test/controllers/cart_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CartControllerTest < ActionDispatch::IntegrationTest
-  test "should get add" do
-    get cart_add_url
-    assert_response :success
-  end
-
-end
--- test/controllers/js_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class JsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
